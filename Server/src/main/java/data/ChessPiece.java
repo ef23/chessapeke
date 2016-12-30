5ffd0f5daf332f53983e3a3fe324c3d9440eb2a8
Chris DeCarolis <cdguitar817@gmail.com>
2016-12-29 19:37:09 -0500

    Finished pawn class, made changes to ChessPiece member variables and added static method

--- Server/src/main/java/data/ChessPiece.java
+++ Server/src/main/java/data/ChessPiece.java
@@ -8,10 +8,23 @@ import json.*;
  */
 public abstract class ChessPiece {
 	protected Space position;//Stores position of piece
+	
+	protected String pieceType;//Stores type of piece
+	
 	protected int[] movement;//Different ways piece can move; row=1s pos, col=10s
+	
 	protected boolean multiple;//Can this piece move more than one predefined movement
 	protected boolean color;//White=true;black=false
+	
 	protected ChessBoard chessBoard;//Stores reference copy 
+	
 	public abstract updateBoard move(Space newPosition);
-	public abstract ArrayList<Space> getMoves(Space position);
+	public abstract ArrayList<Space> getMoves();
+	
+	
+	
+	public static boolean isOutOfBounds(int rowCol)
+	{//Checks if row/column combo is out of bounds
+		return (rowCol%10<=8&&rowCol/10<=8&&rowCol%10>=1&&rowCol/10>=1)?false:true;//checks to make sure both are within bounds
+	}
 }
--- Server/src/main/java/data/Pawn.java
+++ Server/src/main/java/data/Pawn.java
@@ -5,8 +5,9 @@ import json.*;
 
 public class Pawn extends ChessPiece {
 
-	private int attackMovement=11;//attack pattern for pawns
+	private int[] attackMovement=new int[]{11};//attack pattern for pawns
 	public Pawn(Space Position, ChessBoard chessBoard, boolean color){
+		pieceType="pawn";
 		position=Position;
 		movement=new int[]{10};//Movement pattern for pawn
 		chessBoard=chessBoard;//Reference copy of chess board
@@ -16,19 +17,31 @@ public class Pawn extends ChessPiece {
 		//changes position to new position
 		position=newPosition;
 		return null;//Will be changed
-	
 	}
 
 	@Override
-	public ArrayList<Space> getMoves(Space position){
+	public ArrayList<Space> getMoves(){
 		ArrayList<Space> validMoves=new ArrayList<Space>();
 		for(int change:movement)
 		{
 			int row=(position.getSpaceName()%10)+(change%10);
 			int col=(position.getSpaceName()/10)+(change/10);
 			int newRowCol=row+(col*10);//Row/col we are checking
-			if(chessBoard.getPosition(new Space(newRowCol))==null)
+			if(chessBoard.getPosition(new Space(newRowCol))==null&&!isOutOfBounds(newRowCol))
 				validMoves.add(new Space(newRowCol));
 		}
+		for(int change:attackMovement)
+		{
+			int row=(position.getSpaceName()%10)+(change%10);
+			int col1=(position.getSpaceName()/10)+(change/10);
+			int col2=(position.getSpaceName()/10)-(change/10);
+			int newRowCol1=row+(col1*10);
+			if(chessBoard.getPosition(new Space(newRowCol1))!=null&&!isOutOfBounds(newRowCol1))
+				validMoves.add(new Space(newRowCol1));
+			int newRowCol2=row+(col2*10);
+			if(chessBoard.getPosition(new Space(newRowCol2))!=null&&!isOutOfBounds(newRowCol2))
+				validMoves.add(new Space(newRowCol2));
+		}
+		return validMoves;
 	}
 }
